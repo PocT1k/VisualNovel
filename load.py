0a496186ae955d0f31b10af16a97f53e92b79fa5
Kirill Novikov <novikovk2003@yandex.ru>
2024-01-28 02:53:14 +0300

    1

--- load.py
+++ load.py
@@ -6,6 +6,16 @@ def loadingMenu():
     menu = {
         'background': pygame.image.load(path + 'background.png')
     }
+
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Новогодний рассказ")
+    bg_color = (180, 215, 245)
+    screen.fill(bg_color)
+    screen.blit(menu['background'], (0, 0))
+    pygame.display.update()
+
+    pygame.quit()
 pass
 
 def loadingResources():
--- main.py
+++ main.py
@@ -4,14 +4,8 @@ from load import *
 
 
 def run():
+    loadingMenu()
     pygame.init()
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    pygame.display.set_caption("Новогодний рассказ")
-    bg_color = (180, 215, 245)
-    screen.fill(bg_color)
-    screen.blit(menu['background'], (0, 0))
-    pygame.display.update()
-
     # Ожидаем закрытия окна
     running = True
     while running:
